7fd0d6359f34538b6eff2061dbfb7621799d1de3
annzhzho@gmail.com <annzhzho@gmail.com>
2023-05-17 21:41:52 +0300

    add js

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
+const firstPrice = document.querySelector('#firstProductPrice');
+const secondPrice = document.querySelector('#secondProductPrice');
+const thirdPrice = document.querySelector('#thirdProductPrice');
+const fourthPrice = document.querySelector('#fourthProductPrice');
+const discount = 0.8;
 
-let productPrice = document.querySelectorAll('.product__price');
-
-function myFunc() {
-    let sum = 0;
-    let productPrice = document.querySelectorAll('.product__price');
-for( let i = 0; i < productPrice.length; i++){
-    sum += productPrice[i]
-}
-}
-console.log( myFunc());
+const sumPrice  = Number(firstPrice.textContent) + Number(secondPrice.textContent) + Number(thirdPrice.textContent) + Number(fourthPrice.textContent);
 
 let totalProductPrice = document.querySelector('#outcome__price');
-totalProductPrice.innerHTML = myFunc();
+totalProductPrice.textContent = sumPrice;
+
+const discountBtn = document.querySelector('.discount-btn');
+
+discountBtn.onclick = function (){
+  totalProductPrice.textContent = sumPrice * discount;
+  firstPrice.textContent = Number(firstPrice.textContent) * discount
+  secondPrice.textContent = Number(secondPrice.textContent) * discount
+  thirdPrice.textContent = Number(thirdPrice.textContent) * discount
+  fourthPrice.textContent = Number(fourthPrice.textContent) * discount
+}
+
