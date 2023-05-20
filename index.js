a6f6f52127f57398a9cbcae0f57dabdce63a43a6
annzhzho@gmail.com <annzhzho@gmail.com>
2023-05-20 18:15:18 +0300

    add calculator

--- index.js
+++ index.js
@@ -1,24 +1,41 @@
-const firstPrice = document.querySelector('#firstProductPrice');
-const secondPrice = document.querySelector('#secondProductPrice');
-const thirdPrice = document.querySelector('#thirdProductPrice');
-const fourthPrice = document.querySelector('#fourthProductPrice');
+const firstPrice = document.querySelector("#firstProductPrice");
+const secondPrice = document.querySelector("#secondProductPrice");
+const thirdPrice = document.querySelector("#thirdProductPrice");
+const fourthPrice = document.querySelector("#fourthProductPrice");
 const discount = 0.8;
 
-const sumPrice  = Number(firstPrice.textContent) + Number(secondPrice.textContent) + Number(thirdPrice.textContent) + Number(fourthPrice.textContent);
+const sumPrice =
+  Number(firstPrice.textContent) +
+  Number(secondPrice.textContent) +
+  Number(thirdPrice.textContent) +
+  Number(fourthPrice.textContent);
 
-let totalProductPrice = document.querySelector('#outcome__price');
+let totalProductPrice = document.querySelector("#outcome__price");
 totalProductPrice.textContent = sumPrice;
 
-const discountBtn = document.querySelector('.discount-btn');
+const discountBtn = document.querySelector(".discount-btn");
 
-discountBtn.addEventListener('click', () => {
-  totalProductPrice.textContent = sumPrice * discount;
-  firstPrice.textContent = Number(firstPrice.textContent) * discount;
-  secondPrice.textContent = Number(secondPrice.textContent) * discount;
-  thirdPrice.textContent = Number(thirdPrice.textContent) * discount;
-  fourthPrice.textContent = Number(fourthPrice.textContent) * discount;
-}
-,{
-  once: true,
-}
-)
+discountBtn.addEventListener(
+  "click",
+  () => {
+    totalProductPrice.textContent = sumPrice * discount;
+    firstPrice.textContent = Number(firstPrice.textContent) * discount;
+    secondPrice.textContent = Number(secondPrice.textContent) * discount;
+    thirdPrice.textContent = Number(thirdPrice.textContent) * discount;
+    fourthPrice.textContent = Number(fourthPrice.textContent) * discount;
+  },
+  {
+    once: true,
+  }
+);
+
+let enterNumber = document.querySelector("#input-enter");
+let showResult = document.querySelector("#input-result");
+let calcBtn = document.querySelector("#calc-btn");
+
+const calcResult = () => {
+  const result = Math.pow(enterNumber.value, 2);
+  showResult.value = result;
+};
+
+calcBtn.onclick = calcResult;
